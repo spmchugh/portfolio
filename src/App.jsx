b58b20e10cdc0824bb740e61dbe1e594dd96b23d
spmchugh <mchughseanp@gmail.com>
2025-05-20 21:13:09 -0500

    Created a responsive navbar with a desktop navbar and mobile menu.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css"
+import { Navbar } from "./components/Navbar"
+import { MobileMenu } from "./components/MobileMenu"
+import "./index.css"
 
 function App() {
 
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
-    <></>
+    <>
+      <Navbar menuOpen = {menuOpen} setMenuOpen = {setMenuOpen}/>
+      <MobileMenu menuOpen = {menuOpen} setMenuOpen = {setMenuOpen}/>
+    </>
   )
 }
 
